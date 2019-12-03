9d7ec61bdedecffed19ddab7cba61084aa69df11
Iris Johnson <iris@modwiz.com>
2019-12-03 19:26:35 +0000

    Works enough to test

--- spiloop.c
+++ spiloop.c
@@ -14,12 +14,31 @@ static int virtual_spi_one_transfer(struct spi_master *master,
 					struct spi_device *spi,
 					struct spi_transfer *xfer)
 {
+	if (!xfer) {
+		return -EIO;
+		spi_finalize_current_transfer(conn);
+	}
+
+	if (xfer->rx_buf && (xfer->len >= 1)) {
+		uint8_t *rx = (uint8_t*) xfer->rx_buf;
+		rx[0] = 'a';
+	}
 	spi_finalize_current_transfer(conn);
 	return 0;
 }
 
+static struct spi_board_info fake_device_info =
+{
+	.modalias = "spidev",
+	.max_speed_hz = 1,
+	.bus_num = 0,
+	.chip_select = 0,
+	.mode = SPI_MODE_0
+};
+
 static int __init controller_init(void) {
 	int status;
+	struct spi_device *dev;
 	status = misc_register(&fake_controller);
 	if (status) {
 		return status;
@@ -34,6 +53,11 @@ static int __init controller_init(void) {
 	conn->transfer_one = virtual_spi_one_transfer;
 	status = spi_register_controller(conn);
 	if (status) {
+		goto out_free;
+	}
+	dev = spi_new_device(conn, &fake_device_info);
+	if (!dev) {
+		status = -1;
 		goto out_free;
 	}
 	return status;
